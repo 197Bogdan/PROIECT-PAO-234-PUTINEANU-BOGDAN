01ccf1ce47898b392a2738ded95509848ecb1906
197Bogdan <94795061+197Bogdan@users.noreply.github.com>
2023-04-11 23:21:03 +0300

    Update Map.java

--- model/Map.java
+++ model/Map.java
@@ -13,6 +13,21 @@ public class Map {
             }
         }
     }
+    public void initialize(List<Player> players) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                sections[x*height + y] = new MapSection(x, y);
+            }
+        }
+        for (Player player : players) {
+            int x = (int) (Math.random() * width);
+            int y = (int) (Math.random() * height);
+            MapSection section = getSection(x, y);
+            section.addPlayer(player);
+            player.setSection(section);
+        }
+    }
+
 }
 
     
