51a97b38379badb5f266193b6b4e64916ecc2c7d
197Bogdan <94795061+197Bogdan@users.noreply.github.com>
2023-04-11 23:03:50 +0300

    Create Map.java

---
+++ model/Map.java
@@ -0,0 +1,17 @@
+public class Map {
+    private int width;
+    private int height;
+    private List<MapSection> sections;
+
+    public Map(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.sections = new ArrayList<>();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                sections.add(new MapSection(x, y));
+            }
+        }
+    }
+
+    public void initialize(List<Player> players
